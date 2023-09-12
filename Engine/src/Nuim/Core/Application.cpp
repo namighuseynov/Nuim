e2142f96fa048d0b407d9e2ae7ea99f20a0293f9
Namik Guseinoff <94350345+namikguseinoff@users.noreply.github.com>
2023-09-12 16:23:59 +0400

    Fixes

--- Engine/src/Nuim/Core/Application.cpp
+++ Engine/src/Nuim/Core/Application.cpp
@@ -58,6 +58,66 @@ namespace Nuim {
 		{
 			glClear(GL_COLOR_BUFFER_BIT);
 
+
+			float vertices[] = {
+				-0.5f, -0.5f, 0.0f,
+				 0.0f,  0.5f, 0.0f,
+				 0.5f, -0.5f, 0.0f
+			};
+			unsigned int VBO;
+			glGenBuffers(1, &VBO);
+			glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);	
+
+			const char* vertexShaderSource = "#version 330 core\n"
+				"layout (location = 0) in vec3 aPos;\n"
+				"void main()\n"
+				"{\n"
+				"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+				"}\0";
+
+			const char* fragmentShaderSource = "#version 330 core\n"
+				"out vec4 FragColor;\n"
+				"void main()\n"
+				"{\n"
+				"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+				"}\n";
+
+			unsigned int vertexShader;
+			vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+			glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+			glCompileShader(vertexShader);
+			int  success;
+			char infoLog[512];
+			glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+			if (!success)
+			{
+				glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+				std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+			}
+
+			unsigned int fragmentShader;
+			fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+			glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+			glCompileShader(fragmentShader);
+
+			unsigned int shaderProgram;
+			shaderProgram = glCreateProgram();
+
+			glAttachShader(shaderProgram, vertexShader);
+			glAttachShader(shaderProgram, fragmentShader);
+			glLinkProgram(shaderProgram);
+
+			glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+			if (!success) {
+				glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+			}
+
+			glUseProgram(shaderProgram);
+
 			ImGui_ImplOpenGL3_NewFrame();
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
@@ -74,9 +134,21 @@ namespace Nuim {
 			}
 
 
-			ImGui::Begin("Debug");
-			ImGui::Text("Some messages");
-			ImGui::End();
+			ImGui::Begin("Debug"); {
+				if (ImGui::IsWindowHovered()) {
+					ImGui::Text("Some messages");
+					
+				}
+				ImGui::End();
+				
+			}
+			
+			ImGui::Begin("Scene"); {
+				ImGui::Text("Scene");
+				ImGui::End();
+			}
+
+
 
 
 
