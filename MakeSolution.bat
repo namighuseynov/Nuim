c4fe07bb6fdc19bd106bdfe3814534e968111424
Namig Huseynov <94350345+namighuseynov@users.noreply.github.com>
2023-09-29 11:41:18 +0400

    Update MakeSolution.bat

--- MakeSolution.bat
+++ MakeSolution.bat
@@ -1,2 +1,9 @@
+DEL /S *.sln
+DEL /S /Q *.vcxproj
+DEL /S /Q *.vcxproj.filters
+DEL /S /Q *.vcxproj.user
+rmdir /s /q bin
+rmdir /s /q bin-obj
+rmdir /s /q .vs
+
 vendor\premake\premake5.exe --file=premake5.lua vs2022
-PAUSE
