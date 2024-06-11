05381efac2990a2fe7816043b8e5cc8cd66831bd
Namig Huseynov <94350345+namighuseynov@users.noreply.github.com>
2024-06-11 16:40:27 +0400

    Added Demo version

---
+++ Demo/MakeDemoSolution.bat
@@ -0,0 +1,10 @@
+DEL /S *.sln
+DEL /S /Q *.vcxproj
+DEL /S /Q *.vcxproj.filters
+DEL /S /Q *.vcxproj.user
+rmdir /s /q bin
+rmdir /s /q bin-obj
+rmdir /s /q .vs
+
+premake\premake5.exe --file=premake5.lua vs2022
+PAUSE
---
+++ Demo/premake5.lua
@@ -0,0 +1,40 @@
+workspace "NuimDemo"
+    system "windows"
+    systemversion "latest"
+    configurations { "Debug", "Release" }
+    architecture "x64"
+    startproject "NuimDemo"
+    characterset "Unicode"
+    defines {"_CRT_SECURE_NO_WARNINGS", "NUIM_CHARACTERSET_UNICODE" }
+    flags
+	{
+		"MultiProcessorCompile"
+	}
+    
+    filter "configurations:Debug"
+        defines { "DEBUG", "NUIM_DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG", "NUIM_NDEBUG" }
+        optimize "On"
+
+project "NuimDemo"
+    kind "ConsoleApp"
+    -- kind "WindowedApp"
+    language "C++"
+    cppdialect "C++17"
+    targetdir "%{wks.location}/bin"
+    objdir "%{wks.location}/bin-obj"
+    
+    files {
+        "%{wks.location}/src/*.cpp",
+        "%{wks.location}/src/*.h",
+        "%{wks.location}/src/*.c",
+    }
+
+    filter "kind:WindowedApp"
+        defines {"NUIM_SUBSYSTEM_WINDOWS"}
+    filter "kind:ConsoleApp"
+        defines {"NUIM_SUBSYSTEM_CONSOLE"}
+        
