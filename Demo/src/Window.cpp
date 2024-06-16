3bc1f1bf09fb00a9ed61c227479777dcb9ca9ed7
Namig Huseynov <namiq.guseinof@gmail.com>
2024-06-16 22:32:58 +0400

    Window classes

---
+++ Demo/src/Window.cpp
@@ -0,0 +1,18 @@
+#include "Window.h"
+
+namespace NuimDemo {
+	Window::Window(HINSTANCE hInstance, LPWSTR className, UINT16 width, UINT16 height, UINT16 posX, UINT16 posY) {
+		this->hInstance = hInstance;
+		this->className = className;
+		this->Width = width;
+		this->Height = height;
+		this->PosX = posX;
+		this->PosY = posY;
+		wcex = {};
+		wcex.hInstance = this->hInstance;
+		wcex.lpszClassName = this->className;
+		//wcex.lpfnWndProc = this->WndProc;
+		this->hWnd = CreateWindowEx(0, className, className, WS_OVERLAPPEDWINDOW, posX, posY, width, height, nullptr, nullptr, hInstance, nullptr);
+
+	}
+}
